c58bcfd0be30aa60739c447e19aa206e7264143b
Richard Crozier <richard.crozier@yahoo.co.uk>
2014-08-30 19:49:46 +0100

    cube.m added operators

--- +csg/cube.m
+++ +csg/cube.m
@@ -13,6 +13,17 @@ classdef cube < csg.polyhedron
             this.makebox (x, y, z, is_centered);
             
         end
+        
+        % Operators
+        function r = plus (a, b)
+            % call parent class method
+            r = plus@csg.polyhedron (a,b);
+        end
+        
+        function r = minus (a, b)
+            % call parent class method
+            r = minus@csg.polyhedron (a,b);
+        end
     
     end
 
