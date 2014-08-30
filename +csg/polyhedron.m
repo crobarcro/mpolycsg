44df0e49c2d717b1dc1d55ad1639835a2419a955
Richard Crozier <richard.crozier@yahoo.co.uk>
2014-08-30 19:49:06 +0100

    polyhedron.m: added extrude_rotate

--- +csg/polyhedron.m
+++ +csg/polyhedron.m
@@ -1,5 +1,45 @@
 classdef polyhedron < cppinterface
     % base class for computational solid geometry objects
+    %
+    % Syntax
+    %
+    % csg.polyhedron()
+    % csg.polyhedron(p)
+    %
+    % Description
+    %
+    % polyhedron() creates a new polyhedron class without any geometry
+    % information.
+    %
+    % polyhedron(p) creates a new which is a copy of an existing polyhedron
+    % (or derived class) providede in 'p'.
+    %
+    %
+    % polyhedron Methods:
+    %   makeextrusion
+    %   make_surface_of_revolution
+    %   makebox
+    %   makecylinder
+    %   makesphere
+    %   makecone
+    %   maketorus
+    %   render
+    %   union
+    %   difference
+    %   symmetric_difference
+    %   translate
+    %   rotate
+    %   scale
+    %   rotmat
+    %   transform
+    %   num_vertices
+    %   num_faces
+    %   num_face_vertices
+    %   get_vertices
+    %   get_vertex
+    %   get_face_vertices
+    %   triangulate
+    %
 
     methods
     
@@ -10,11 +50,21 @@ classdef polyhedron < cppinterface
             
             if nargin > 0
                 
-                if isa (other, 'csg.polyhedron')
-                    % TODO: what about derived classes?
-                    this.cppcall ('copy', other.objectHandle);
-                else
-                    error ('Inuput arg was not derived from csg.polyhedron. Could not copy.');
+                if isoctave
+                    % TODO: when octave implements 'superclasses', use it
+                    % for it too
+                    if strncmpi (class(other), 'csg', 3)
+                        % TODO: what about derived classes?
+                        this.cppcall ('copy', other.objectHandle);
+                    else
+                        error ('Inuput arg was not derived from csg.polyhedron. Could not copy.');
+                    end
+                else 
+                    if isa (other, 'csg.polyhedron') || any (strcmp (superclasses (other), 'csg.polyhedron'))
+                        this.cppcall ('copy', other.objectHandle);
+                    else
+                        error ('Inuput arg was not derived from csg.polyhedron. Could not copy.');
+                    end
                 end
             end            
             
@@ -46,15 +96,64 @@ classdef polyhedron < cppinterface
         end
         
         function verts = get_face_vertices (this, face_id)
-        
+            % returns the indices of the vertices of a face
+
             verts = this.cppcall ('get_face_vertices', face_id);
             
         end
         
-        % shapes
-        function makeextrusion (this, nodes, links, distance)
+        function verts = get_vertices (this)
+            % returns all the vertices from the polyhedron
             
-            this.cppcall ('extrusion', nodes, links, distance);
+            n = num_vertices (this);
+            verts = ones (n,3) * nan;
+            for ind = 1:n
+                verts(ind,:) =  get_vertex (this, ind-1);
+            end
+            
+        end
+        
+        % shapes
+        function make_extrusion (this, distance, nodes, links)
+            % create solid from extruded polygon
+            %
+            % Syntax
+            %
+            % polyhedron/make_extrusion  (distance, nodes)
+            % polyhedron/make_extrusion  (distance, nodes, links)
+            %
+            % Input
+            %
+            %   
+            
+            if nargin < 4
+                links = 0:size(nodes, 1)-1;
+%                 links = [links, 0];
+            end
+            
+            this.cppcall ('extrusion', nodes, links(:), distance);
+              
+        end
+        
+        % shapes
+        function extrude_rotate (this, distance, segments, dTheta, nodes, links)
+            % create solid from extruded polygon
+            %
+            % Syntax
+            %
+            % polyhedron/make_extrusion  (distance, segments, dTheta, nodes)
+            % polyhedron/make_extrusion  (distance, segments, dTheta, nodes, links)
+            %
+            % Input
+            %
+            %   
+            
+            if nargin < 4
+                links = 0:size(nodes, 1)-1;
+%                 links = [links, 0];
+            end
+            
+            this.cppcall ('extrude_rotate', nodes, links(:), distance, segments, dTheta);
               
         end
         
@@ -160,7 +259,7 @@ classdef polyhedron < cppinterface
 
         function rotate (this, rvector)
             if (~isnumeric (rvector)) || (numel(rvector) ~= 3)
-                error ('Rotation vector must be 3 element numeric translation vector [theta_x, theta_y, theta_z]');
+                error ('Rotation vector must be 3 element numeric rotation vector [theta_x, theta_y, theta_z]');
             end
             
             % convert from radians to degrees
@@ -219,6 +318,34 @@ classdef polyhedron < cppinterface
             
         end
         
+        function varargout = triangulate (this)
+            % triangulate the surface of the polyhedron, optionally returning
+            % all vertices and faces
+            %
+            % Syntax
+            %
+            % polyhedron/triangulate (this)
+            % [nodes, vertinds] = triangulate (this)
+            %
+            % 
+            
+            this.cppcall ('triangulate');
+            
+            if nargout > 0
+                varargout{1} = this.get_vertices ();
+                
+            end
+            
+            if nargout > 1
+                n = this.num_faces ();
+                verts = ones (n,3) * nan;
+                for faceind = 1:n
+                    verts(faceind,:) = get_face_vertices (this, faceind-1);
+                end
+                varargout{2} = verts;
+            end 
+        end
+        
         % Operators
         function r = plus (a, b)
         
@@ -238,11 +365,25 @@ classdef polyhedron < cppinterface
             
         end
         
-        
-        
         % Drawing
-        function render (this)
+        function render (this, pretty)
+            % plot the polyhedron
+            %
+            % Syntax
+            %
+            % polyhedron/render ()
+            % polyhedron/render (pretty)
+            %
+            % Input
+            %
+            %  pretty - optional flag, if true, edges will not be drawn and
+            %    lighting added. Default is false
+            %
         
+            if nargin < 2
+                pretty = false;
+            end
+            
             hfig = figure;
             hax = axes;
             
@@ -260,20 +401,170 @@ classdef polyhedron < cppinterface
                 
                 end
                 
-                % draw the patch 
-                patch ( facecoords(:,1), ...
-                        facecoords(:,2), ...
-                        facecoords(:,3), ...
-                        [] );
+                if pretty
+                    % draw the patch 
+                    p = patch ( facecoords(:,1), ...
+                                facecoords(:,2), ...
+                                facecoords(:,3), 'r');
+
+                    set (p, 'EdgeColor','none');
+                    
+                else
+                    % draw the patch 
+                    patch ( facecoords(:,1), ...
+                            facecoords(:,2), ...
+                            facecoords(:,3), ...
+                            'r' );
+                end
             
             end
             
+            view(3);
+            axis equal;
+            if pretty, light; end
             xlabel ('x');
             ylabel ('y');
             zlabel ('z');
+            
         
         end
         
+        
+        function stlwrite (this, filename)
+            % triangulate and write out polyhedron to stl file
+            % 
+            % Syntax
+            %
+            % polyhedron/stlwrite (filename)
+            %
+            
+            [fv.vertices, fv.faces] = this.triangulate ();
+            fv.faces = fv.faces + 1;
+
+            stlwrite(filename,fv);
+        
+        end
+        
+        function objwrite (this, filename)
+            % VERTFACE2OBJ Save a set of vertice coordinates and faces as a Wavefront/Alias Obj file
+
+            fid = fopen(filename,'w');
+            
+            % write out the vertices
+            nverts = this.num_vertices ();
+
+            for vert = 0:nverts-1
+                v = this.get_vertex (vert);
+                fprintf(fid,'v %f %f %f\n',v(1),v(2),v(3));
+            end
+
+            n = this.num_faces ();
+            
+            for face_id = 0:n-1
+                
+                verts = this.get_face_vertices (face_id);
+                
+                fprintf(fid,'f ');
+                for indii = 1:numel(verts)
+                    fprintf(fid,'%d ', verts(indii)+1);
+                end
+                
+                fprintf(fid,'\n');
+            
+            end
+            
+            fprintf(fid,'g\n');
+
+            fclose(fid);
+            
+        end
+        
+        function offwrite (this, filename)
+            % write the polyhedron to an OFF file
+            
+            % open the output file and check for success
+            fid = fopen(filename,'w');
+
+            % compute the number of vertices and faces
+            nverts = this.num_vertices ();
+            nfaces = this.num_faces ();
+
+            % write out the header
+            fprintf(fid, 'OFF\n');
+            fprintf(fid, '%d %d 0\n', nverts, nfaces );
+
+            % write out the vertices
+            for vert = 0:nverts-1
+                v = this.get_vertex (vert);
+                fprintf(fid,'%f %f %f\n',v(1),v(2),v(3));
+            end
+
+            % write out the faces
+            for face_id = 0:nfaces-1
+                
+                verts = this.get_face_vertices (face_id);
+                
+                fprintf(fid,'%d ', numel (verts));
+                for indii = 1:numel(verts)
+                    fprintf(fid,'%d ', verts(indii));
+                end
+                
+                fprintf(fid,'\n');
+            
+            end
+
+            % close the output file
+            fclose(fid);
+            
+        end
+        
+        function plywrite (this, filename)
+            % write the polyhedron to an OFF file
+            
+            % open the output file and check for success
+            fid = fopen(filename,'w');
+            
+
+            % compute the number of vertices and faces
+            nverts = this.num_vertices ();
+            nfaces = this.num_faces ();
+
+            % write out the header
+            fprintf(fid, 'ply\n');
+            fprintf(fid, 'format ascii 1.0\n');
+            fprintf(fid, 'comment ply file gnerated by mpolycsg\n');
+            fprintf(fid, 'element vertex %d\n', nverts);
+            fprintf(fid, 'property float x\n');
+            fprintf(fid, 'property float y\n');
+            fprintf(fid, 'property float z\n');
+            fprintf(fid, 'element face %d\n', nfaces);
+            fprintf(fid, 'property list uchar int vertex_index\n' );
+            fprintf(fid, 'end_header\n');
+
+            % write out the vertices
+            for vert = 0:nverts-1
+                v = this.get_vertex (vert);
+                fprintf(fid,'%f %f %f\n',v(1),v(2),v(3));
+            end
+
+            % write out the faces
+            for face_id = 0:nfaces-1
+                
+                verts = this.get_face_vertices (face_id);
+                
+                fprintf(fid,'%d ', numel (verts));
+                for indii = 1:numel(verts)
+                    fprintf(fid,'%d ', verts(indii));
+                end
+                
+                fprintf(fid,'\n');
+            
+            end
+
+            % close the output file
+            fclose(fid);
+            
+        end
     
     end
 
